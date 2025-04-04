3de6da015720a29b731c37e2da9d4c201e2681c1
Abhishek Dalbanjan <abhishekdalbanjan1@gmail.com>
2025-04-04 12:41:24 +0530

    Added transfer query

--- db/query/account.sql
+++ db/query/account.sql
@@ -26,3 +26,16 @@ WHERE id = $1;
 -- name: DeleteAccount :exec
 DELETE FROM accounts
 WHERE id = $1;
+
+-- name: GetTransfer :one
+SELECT * FROM transfers
+WHERE id = $1 LIMIT 1;
+
+-- name: ListTransfers :many
+SELECT * FROM transfers
+WHERE 
+    from_account_id = $1 OR
+    to_account_id = $2
+ORDER BY id
+LIMIT $3
+OFFSET $4;
